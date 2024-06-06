736639d4e84e133d468d0368cca11a10eacf3ed1
A A S <rizkynhg@gmail.com>
2024-06-06 11:30:48 +0700

    installation dan configuration vue star rating

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/style.min.css', '@/assets/css/custom.css'],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vue-star-rating.js', mode: 'client' }],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
