d0e52553bfe1014f08178af803776a82b9fc1979
A A S <rizkynhg@gmail.com>
2024-06-06 10:21:30 +0700

    installation template dashboard coreui

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ export default {
   target: 'server',
   // rendering mode SSR
   ssr: true,
-
   loading: {
     color: 'white', // <-- color
     height: '5px', // <-- height
@@ -15,29 +14,50 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/images/logo.png',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
+      },
+    ],
+    script: [
+      { src: '/js/coreui.bundle.min.js' },
+      {
+        src: 'https://app.sandbox.midtrans.com/snap/snap.js',
+        'dataclient-key': 'SB-Mid-client-M5HF-xGXMyM84qmF',
+      },
+    ],
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
+  css: ['@/assets/css/style.min.css', '@/assets/css/custom.css'],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
+  buildModules: [],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
@@ -45,12 +65,10 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseUrl: 'http://127.0.0.1:8000',
+    baseURL: 'http://localhost:8000',
   },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
