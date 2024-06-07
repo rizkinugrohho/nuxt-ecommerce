3ceb7d7218a76fd48f9d13562ab47d5f4aa0c521
A A S <rizkynhg@gmail.com>
2024-06-07 11:15:22 +0700

    configuration vuex admin products

---
+++ store/admin/product.js
@@ -0,0 +1,41 @@
+// state
+export const state = () => ({
+  // products
+  products: [],
+  // page
+  page: 1,
+})
+// mutations
+export const mutations = {
+  // mutation "SET_PRODUCTS_DATA"
+  SET_PRODUCTS_DATA(state, payload) {
+    // set value state "products"
+    state.products = payload
+  },
+  // mutation "SET_PAGE"
+  SET_PAGE(state, payload) {
+    // set value state "page"
+    state.page = payload
+  },
+}
+// actions
+export const actions = {
+  // get products data
+  getProductsData({ commit, state }, payload) {
+    // search
+    let search = payload ? payload : ''
+    // set promise
+    return new Promise((resolve, reject) => {
+      // fetching Rest API "/api/admin/products" with method "GET"
+      this.$axios
+        .get(`/api/admin/products?q=${search}&page=${state.page}`)
+        // success
+        .then((response) => {
+          // commit ti mutation "SET_PRODUCTS_DATA"
+          commit('SET_PRODUCTS_DATA', response.data.data)
+          // resolve promise
+          resolve()
+        })
+    })
+  },
+}
