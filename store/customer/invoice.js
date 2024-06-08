6d42d3609e4787a1b052c85e87f6eb167ddbb91d
A A S <rizkynhg@gmail.com>
2024-06-08 13:22:15 +0700

    configuration vuex customer invoices

---
+++ store/customer/invoice.js
@@ -0,0 +1,42 @@
+// state
+export const state = () => ({
+  // invoices
+  invoices: [],
+  // page
+  page: 1,
+})
+// mutations
+export const mutations = {
+  // mutation "SET_INVOICES_DATA"
+  SET_INVOICES_DATA(state, payload) {
+    // set value state "invoices"
+    state.invoices = payload
+  },
+  // mutation "SET_PAGE"
+  SET_PAGE(state, payload) {
+    // set value state "page"
+    state.page = payload
+  },
+}
+// actions
+export const actions = {
+  // get invoices data
+  getInvoicesData({ commit, state }, payload) {
+    // search
+    const search = payload || ''
+    // set promise
+    return new Promise((resolve, reject) => {
+      // fetching Rest API "/api/customer/invoices" with method
+      'GET'
+      this.$axios
+        .get(`/api/customer/invoices?q=${search}&page=${state.page}`)
+        // success
+        .then((response) => {
+          // commit ti mutation "SET_INVOICES_DATA"
+          commit('SET_INVOICES_DATA', response.data.data)
+          // resolve promise
+          resolve()
+        })
+    })
+  },
+}
