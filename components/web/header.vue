765f1399b3fcaa7cfe97effd44e3c0d020434b10
A A S <rizkynhg@gmail.com>
2024-06-10 16:20:23 +0700

    update header

--- components/web/header.vue
+++ components/web/header.vue
@@ -6,7 +6,7 @@
                     <div class="col-lg-3 col-sm-4 col-md-4 col-5">
                         <nuxt-link to="/" class="brand-wrap" data-abc="true">
                             <img src="/images/xiaomi.png" width="35" class="bg-light p-2 rounded">
-                            <span class="logo">MI STORE</span>
+                            <span class="logo">SHOE STORE</span>
                         </nuxt-link>
                     </div>
                     <div class="col-lg-4 col-xl-5 col-sm-8 col-md-4 d-none d-md-block">
